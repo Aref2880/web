1574dc9e4df815b7d40eac4a03568fcac633331d
Aref2880 <Aref.delshab2880@gmail.com>
2024-05-26 21:26:12 +0330

    Update BMI.js

--- src/Components/BMI.js
+++ src/Components/BMI.js
@@ -1,5 +1,28 @@
 const BMI = {} =>{
 
+    return (
+        <div>
+            <h1>BMI Calculator</h1>
+
+            <form>
+                <div>
+                    <label>Age:</label><br/>
+                    <input type="number" placeholder="Age..."/>
+                </div>
+
+                <div>
+                    <label>Weight:</label><br/>
+                    <input type="number" placeholder="Weight..."/>
+                </div>
+
+                <div>
+                    <label>Hight:</label><br/>
+                    <input type="number" placeholder="Height..."/>
+                </div>
+
+            </form>
+        </div>
+    )
 }
 
-export default BMI
+export default BMI;
